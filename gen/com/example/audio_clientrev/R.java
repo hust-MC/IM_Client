4a576575502ac97ca95312f747c9bad465a2601e
mc <mc_zy@outlook.com>
2014-04-03 17:43:28 +0800

    delete extra

--- gen/com/example/audio_clientrev/R.java
+++ gen/com/example/audio_clientrev/R.java
@@ -29,14 +29,16 @@ public final class R {
         public static final int button=0x7f020005;
         public static final int ic_launcher=0x7f020006;
         public static final int launcher=0x7f020007;
-        public static final int shutdown=0x7f020008;
+        public static final int newusr=0x7f020008;
+        public static final int shutdown=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
-        public static final int chatView_l=0x7f080002;
-        public static final int chatView_r=0x7f080003;
+        public static final int action_settings=0x7f080005;
+        public static final int chatView_l=0x7f080003;
+        public static final int chatView_r=0x7f080004;
+        public static final int inputMessage=0x7f080001;
         public static final int lv=0x7f080000;
-        public static final int send_bt=0x7f080001;
+        public static final int send_bt=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/audio_clientrev/MainActivity.java
+++ src/com/example/audio_clientrev/MainActivity.java
@@ -23,47 +23,46 @@ import android.widget.*;
 
 public class MainActivity extends Activity
 {
-	static int i=0;
+	static int i = 0;
 	public static String[] data = new String[]
-			{
-			"天青色等烟雨",
-			"而我在等你",
-			"月色被打捞起",
-			"晕开了结局",
-			"如传世的青花瓷",
-			"自顾自美丽",
-			"你眼带",
-			"笑意"
-			};
-	
+	{ "天青色等烟雨", "而我在等你", "月色被打捞起", "晕开了结局", "如传世的青花瓷", "自顾自美丽", "你眼带", "笑意" };
+
 	Socket socket = null;
-	EditText inputIP, inputSms;
-	Button button;
+	EditText inputMessage;
+	Button send_bt;
 	ListView lv;
+	Handler handler;
 	ChatAdapter chatAdapter;
-	LinearLayout alertDialog;
+
+	public void wiget_init()
+	{
+		lv = (ListView) findViewById(R.id.lv);
+		inputMessage = (EditText) findViewById(R.id.inputMessage);
+		send_bt = (Button) findViewById(R.id.send_bt);
+	}
 
 	public void onClick_send(View view)
 	{
-		
-		chatAdapter.addText(data[i], true);
-		i++;
+		chatAdapter.addText(inputMessage.getText().toString(), true);
 	}
-	
-	public void onClick_rev(View view)
-	{
-		
-		chatAdapter.addText(data[i], false);
-		i++;
-	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		lv = (ListView)findViewById(R.id.lv);
+
+		wiget_init();
 		chatAdapter = new ChatAdapter(this);
 		lv.setAdapter(chatAdapter);
+		handler = new Handler()
+		{
+			@Override
+			public void handleMessage(Message msg)
+			{
+				chatAdapter.addText(inputMessage.getText().toString(), false);
+			}
+		};
 	}
 
 	@Override
